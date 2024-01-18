31c389f8147e8dfc21fffa6fe5e24b17e3be85b9
16721237@mahasiswa.itb.ac.id <16721237@mahasiswa.itb.ac.id>
2024-01-18 12:56:28 +0700

    feat: add google auth (bismillah)

--- go.mod
+++ go.mod
@@ -17,7 +17,15 @@ require (
 )
 
 require (
+	cloud.google.com/go/compute v1.23.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -31,9 +39,17 @@ require (
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/oauth2 v0.16.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
+	google.golang.org/api v0.156.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917 // indirect
 )
--- service/auth.go
+++ service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"os"
 
 	"github.com/Reigenleif/ecomate-mobile-backend-service/internal/db"
 	"github.com/Reigenleif/ecomate-mobile-backend-service/internal/token_service"
@@ -11,6 +12,8 @@ import (
 	"github.com/gobuffalo/nulls"
 	"github.com/gofrs/uuid"
 	"golang.org/x/crypto/bcrypt"
+
+	"google.golang.org/api/idtoken"
 )
 
 type AuthService struct {
@@ -112,7 +115,15 @@ func (s *AuthService) Register(ctx context.Context, req *proto.RegisterRequest) 
 }
 
 func (s *AuthService) GoogleLogin(ctx context.Context, req *proto.LoginRequest) (*proto.UserResponse, error) {
-	rows, err := db.GetDB().Query(ctx, "SELECT * FROM public.\"User\" WHERE email = $1", req.Email)
+	tokenString := req.Email
+	audience := os.Getenv("GOOGLE_CLIENT_ID")
+
+	payload, err := idtoken.Validate(context.Background(), tokenString, audience)
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.GetDB().Query(ctx, "SELECT * FROM public.\"User\" WHERE email = $1", payload.Claims["email"])
 	if err != nil {
 		return nil, err
 	}
@@ -120,7 +131,46 @@ func (s *AuthService) GoogleLogin(ctx context.Context, req *proto.LoginRequest) 
 
 	var user models.User
 	if rows.Next() {
+		// case user already exists
 		err := rows.Scan(&user.ID, &user.Name, &user.Email, nil, &user.HashPassword, &user.ImageUrl, &user.Role)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// case user does not exist
+		hashPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Claims["email"].(string)), 12)
+		if err != nil {
+			return nil, err
+		}
+
+		user = models.User{
+			ID: nulls.String{
+				String: uuid.Must(uuid.NewV4()).String(),
+				Valid:  true,
+			},
+			Name: nulls.String{
+				String: payload.Claims["name"].(string),
+				Valid:  true,
+			},
+			Email: nulls.String{
+				String: payload.Claims["email"].(string),
+				Valid:  true,
+			},
+			HashPassword: nulls.String{
+				String: string(hashPassword),
+				Valid:  true,
+			},
+			ImageUrl: nulls.String{
+				String: payload.Claims["picture"].(string),
+				Valid:  true,
+			},
+			Role: nulls.String{
+				String: "COSTUMER",
+				Valid:  true,
+			},
+		}
+
+		_, err = db.GetDB().Exec(ctx, "INSERT INTO public.\"User\" (id, name, email, \"hashPassword\", \"imageUrl\") VALUES ($1, $2, $3, $4, $5)", user.ID, user.Name, user.Email, user.HashPassword, user.ImageUrl)
 		if err != nil {
 			return nil, err
 		}
